62f43f5bb43438885ee36be710826f735594666a
MarceliTx <74084164+MarceliTx@users.noreply.github.com>
2024-08-27 19:20:11 +0000

    add code in hours.j

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,25 @@
 // Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+    const hours = [];
+    const currentDate = new Date();
+    const currentDay = currentDate.toLocaleString('en-us', { weekday: 'long' });
+
+    for (let i = 0; i < 24; i++) {
+        hours.push(`${i}:00`);
+    }
+
+    return (
+        <div>
+            <h2>Hours of {currentDay}</h2>
+            <ul>
+                {hours.map((hour, index) => (
+                    <li key={index}>{hour}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Hours;
