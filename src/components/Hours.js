1142dcce95113d0d651afcfa866847dba6ddc735
MarceliTx <74084164+MarceliTx@users.noreply.github.com>
2024-08-27 19:30:52 +0000

    grow code to components hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -2,17 +2,29 @@
 import React from 'react';
 
 const Hours = () => {
-    const hours = [];
     const currentDate = new Date();
-    const currentDay = currentDate.toLocaleString('en-us', { weekday: 'long' });
+    const currentDay = currentDate.getDay();
+    const hours = [];
 
-    for (let i = 0; i < 24; i++) {
+    let startHour, endHour;
+
+    if (currentDay >= 1 && currentDay <= 5) {
+        // Lunes a Viernes
+        startHour = 10;
+        endHour = 16;
+    } else {
+        // Sábado y Domingo
+        startHour = 9;
+        endHour = 20;
+    }
+    
+    for (let i = startHour; i <= endHour; i++) {
         hours.push(`${i}:00`);
     }
 
     return (
         <div>
-            <h2>Hours of {currentDay}</h2>
+            <h2>Hours of {currentDate.toLocaleDateString('en-us', { weekday: 'long' })}</h2>
             <ul>
                 {hours.map((hour, index) => (
                     <li key={index}>{hour}</li>
